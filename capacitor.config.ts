4c80116f3e7810ad873b079ccafd7c279a955465
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2023-02-24 01:18:31 +0530

    updating files & debugging

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.rajvansh.angular',
+  appName: 'Angular-Capacitor-App',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
